fc86516e4166817cd0f357621467d6ecda5636f2
dse <dse@ds-s.at>
2022-10-14 17:07:57 +0200

    Initial commit

---
+++ src/main/java/com/example/wordapi/WordApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wordapi/WordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
